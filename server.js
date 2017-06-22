9da5a060a225a25ddcc2035813102ec27faf18f0
Xiaowen Zhang <sean@evestemptation.com>
2017-06-22 11:44:18 -0700

    Add project page

--- server.js
+++ server.js
@@ -60,6 +60,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects',(req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
